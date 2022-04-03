fb45be770a9384d3f4df8e77756acdad40a1e2c8
vnikitenok <vyacheslav.nikitenok@fortegrp.com>
2022-04-03 15:22:04 +0300

    Use import as a function

--- apps/base/baseData.js
+++ apps/base/baseData.js
@@ -9,7 +9,7 @@ if(process.env.NODE_ENV === 'prod') {
 }
 
 import admin from 'firebase-admin';
-import serviceAccount from [serviceAccountPath];
+const serviceAccount = import(serviceAccountPath);
 import CONFIG from './config.js';
 
 class BaseData {
